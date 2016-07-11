fb2d5f9190395d2c44fd632479b39e4f9c468c8c
alfonzm <m.alfonz@gmail.com>
2016-07-11 10:35:15 +0800

    Update window replace on login

--- js/signin.js
+++ js/signin.js
@@ -33,7 +33,7 @@ function logIn(){
 
   account.Login(login_parameters, function(result){
     if(result){
-      window.location.replace('communication');
+      window.location.replace('.');
     }
 
     reset();
